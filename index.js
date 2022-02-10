f4a66a05ed93a33a45a88e96ae4f28f67165713d
ZuguangTong <810732627@qq.com>
2022-02-11 00:22:48 +1100

    Comments API

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ app.use(cors())
 const db = require("./models")
 
 const postRouter = require("./routes/Posts")
-
 app.use("/posts", postRouter)
+
+const commentsRouter = require("./routes/Comments")
+app.use("/comments", commentsRouter)
 
 db.sequelize.sync().then(()=>{
     app.listen(3301, ()=>{
