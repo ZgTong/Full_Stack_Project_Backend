ffa737e0a35f90ec6eb672921c5cc23b7bcbd2b0
ZuguangTong <810732627@qq.com>
2022-02-19 17:15:13 +1100

    delete post

--- routes/Posts.js
+++ routes/Posts.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const { Posts,Likes } = require("../models")
-
+const { Posts, Likes } = require("../models")
+const { validateToken } = require("../middlewares/AuthMiddleware")
 router.get("/", async (req, res) => {
     const listOfPosts = await Posts.findAll({
         include: [Likes]
     })
     res.json(listOfPosts)
 })
-router.post("/", async (req, res) => {
+router.post("/", validateToken, async (req, res) => {
     const post = req.body
-    // console.log("hahahahaha", req);
+    post.username = req.user.username
     await Posts.create(post)
     res.json(post)
 })
@@ -19,6 +19,16 @@ router.get("/byId/:id", async (req, res) => {
     const id = req.params.id
     const post = await Posts.findByPk(id)
     res.json(post)
+})
+
+router.delete(`/:postId`, validateToken, async (req, res) => {
+    const postId = req.params.postId
+    await Posts.destroy({
+        where: {
+            id: postId
+        }
+    })
+    return res.json("delete success")
 })
 
 module.exports = router
